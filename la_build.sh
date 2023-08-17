6d2f70e92e7458cba10806ac05545cc375afd90a
XhyDds <1136759302@qq.com>
2023-08-17 15:55:52 +0800

    creep linux

---
+++ la_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export CROSS_COMPILE=~/gitwork/loongarch32r-linux-gnusf/bin/loongarch32r-linux-gnusf-
+export ARCH=loongarch
+OUT=la_build
+
+if [ ! -d la_build ] ;then
+    mkdir la_build
+    make la32_defconfig O=${OUT}
+fi
+
+echo "----------------output ${OUT}----------------"
+
+make menuconfig O=${OUT}
+make vmlinux -j  O=${OUT} 2>&1 | tee -a build_error.log
